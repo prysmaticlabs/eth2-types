c267a054804e0c8d9165c06b4dbd181db3afd099
Victor Farazdagi <simple.square@gmail.com>
2021-01-26 22:48:37 +0300

    Init module

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package types contains ETH2 data types shared between various parts of the system (beacon chain,
+// validator, slasher).
+package types
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/prysmaticlabs/eth2-types
+
+go 1.15
+
+require github.com/ferranbt/fastssz v0.0.0-20210120143747-11b9eff30ea9
